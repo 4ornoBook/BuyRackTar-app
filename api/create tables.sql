06a646cb1b7368d2d2e7476a65e450aae3937204
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-05-01 23:51:05 +0300

    updated tables fields

--- api/create tables.sql
+++ api/create tables.sql
@@ -29,6 +29,7 @@ create table wallet_transaction (
     id serial primary key,
     from_wallet_id integer references wallet(id),
     to_wallet_id integer references wallet(id),
+    name varchar(255) not null,
     amount money not null,
     time  timestamp default now(),
     description text not null
@@ -38,6 +39,8 @@ create table category (
     id serial primary key,
     accound_id integer references account(id),
     name varchar(255) not null,
+    limit money not null,
+    currency_id integer references currency(id),
     description text not null,
     active boolean not null
 );
