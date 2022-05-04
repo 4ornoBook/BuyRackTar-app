3f0476231f1787f54ea3deb4dba53c75ac13711b
Anton Kuchmii <anton.kuchmiy@cpamatica.io>
2022-05-04 22:13:20 +0300

    feat: injectable id param from active route

--- client/src/app/modules/shared/helpers/routing-helper/routing-helper.service.ts
+++ client/src/app/modules/shared/helpers/routing-helper/routing-helper.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken, Provider } from '@angular/core';
+import { Observable, of, switchMap } from 'rxjs';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
-@Injectable({
-	providedIn: 'root',
-})
+export const ID_FROM_ROUTE = new InjectionToken<Observable<number>>(
+	'A stream with current id param from route'
+);
+
+export const ID_FROM_ROUTE_PROVIDERS: Provider[] = [
+	{
+		provide: ID_FROM_ROUTE,
+		deps: [ActivatedRoute],
+		useFactory: idFromRouteFactory,
+	},
+];
+
+export function idFromRouteFactory(route: ActivatedRoute): Observable<number> {
+	return route.params.pipe(
+		switchMap(params =>
+			of(Number(convertToParamMap(params).get('id')) as unknown as number)
+		)
+	);
+}
+
+@Injectable()
 export class RoutingHelperService {
 	constructor() {}
 
