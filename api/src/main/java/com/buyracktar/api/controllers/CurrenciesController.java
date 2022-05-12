c9fce1eaedaea3a9859148ff712e7f6d48fa79fc
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-05-12 23:21:21 +0300

    optimize currency controller

--- api/src/main/java/com/buyracktar/api/controllers/CurrenciesController.java
+++ api/src/main/java/com/buyracktar/api/controllers/CurrenciesController.java
@@ -3,20 +3,21 @@ package com.buyracktar.api.controllers;
 import com.buyracktar.api.responsemodels.MyResponseTemplate;
 import com.buyracktar.api.entities.Currency;
 import com.buyracktar.api.repositories.CurrencyRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class CurrenciesController {
 
-	@Autowired
-	CurrencyRepository currencyRepository;
+	private final CurrencyRepository currencyRepository;
 
 	@GetMapping("currencies")
-	public MyResponseTemplate getCurrencies() {
+	public ResponseEntity<Object> getCurrencies() {
 		Iterable<Currency> currencies = currencyRepository.findAll();
-		MyResponseTemplate myResponse = new MyResponseTemplate(true, currencies, null);
-		return myResponse;
+		return ResponseEntity.ok(new MyResponseTemplate(true, currencies, null));
 	}
 }
