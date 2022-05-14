f21919df8dd3e0fd7b66b746d2b6b57ddd05617b
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-05-14 20:07:03 +0300

    add wallet controller

---
+++ api/src/main/java/com/buyracktar/api/controllers/WalletController.java
@@ -0,0 +1,9 @@
+package com.buyracktar.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wallets")
+public class WalletController {
+}
