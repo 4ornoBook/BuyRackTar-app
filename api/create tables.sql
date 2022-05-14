22058fbd35746b139d2c690e0ed62191f6c570e2
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-05-14 19:19:35 +0300

    change money type on numeric

--- api/create tables.sql
+++ api/create tables.sql
@@ -22,7 +22,7 @@ create table wallet (
     id serial primary key,
     user_id integer references client(id),
     currency_id integer references currency(id),
-    amount money not null
+    amount numeric(20,2) not null
 );
 
 create table wallet_transaction (
@@ -30,7 +30,7 @@ create table wallet_transaction (
     from_wallet_id integer references wallet(id),
     to_wallet_id integer references wallet(id),
     name varchar(255) not null,
-    amount money not null,
+    amount numeric(20,2) not null,
     time  timestamp default now(),
     description text not null
 );
@@ -39,7 +39,7 @@ create table category (
     id serial primary key,
     account_id integer references account(id),
     name varchar(255) not null,
-    money_limit money not null,
+    money_limit numeric(20,2) not null,
     currency_id integer references currency(id),
     description text not null,
     active boolean not null
@@ -49,7 +49,7 @@ create table category_transaction (
     id serial primary key,
     wallet_id integer references wallet(id),
     category_id integer references category(id),
-    amount money not null,
+    amount numeric(20,2) not null,
     time timestamp default now(),
     description text not null
 );
