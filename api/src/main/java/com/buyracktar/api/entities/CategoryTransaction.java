d3e80606a7db36d96d08af812dab827d9192ed43
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-05-15 20:59:18 +0300

    add category in categoryTransaction

--- api/src/main/java/com/buyracktar/api/entities/CategoryTransaction.java
+++ api/src/main/java/com/buyracktar/api/entities/CategoryTransaction.java
@@ -2,6 +2,7 @@ package com.buyracktar.api.entities;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,22 +12,28 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@JsonPropertyOrder({"id", "wallet", "category", "amount", "time", "description"})
 public class CategoryTransaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne()
-    @JoinColumn(name = "walletId")
+    @JoinColumn(name = "wallet_id")
     private Wallet wallet;
 
-    private Long categoryId;
+    @ManyToOne()
+    @JoinColumn(name = "category_id")
+    @JsonIgnore
+    private Category category;
 
     private BigDecimal amount;
 
@@ -38,5 +45,13 @@ public class CategoryTransaction {
     @JsonGetter("time")
     public long getTime() {
         return Timestamp.valueOf(time).getTime();
+    }
+
+    @JsonGetter("category")
+    public Map<String, String> getJsonCategory() {
+        Map<String, String> categoryMap = new HashMap<>();
+        categoryMap.put("id", category.getId()+"");
+        categoryMap.put("name", category.getName());
+        return categoryMap;
     }
 }
